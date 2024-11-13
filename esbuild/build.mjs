a59a67a0b5313ecc25230ae29aef74cabb88d84e
d3m1d0v <d3m1d0v@yandex-team.ru>
2024-11-13 19:20:49 +0300

    feat: added file plugin and runtime

--- esbuild/build.mjs
+++ esbuild/build.mjs
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 
 import {build} from 'esbuild';
+import {sassPlugin} from 'esbuild-sass-plugin';
 
 import tsConfig from '../tsconfig.json' assert {type: 'json'};
 
@@ -14,9 +15,34 @@ const common = {
     tsconfig: './tsconfig.publish.json',
 };
 
-build({
+/** @type {import('esbuild').BuildOptions}*/
+const plugin = {
     ...common,
-    entryPoints: ['src/index.ts'],
-    outfile: outDir + '/index.js',
+    entryPoints: ['src/plugin/index.ts'],
+    outfile: outDir + '/plugin/index.js',
+    platform: 'node',
     packages: 'external',
-});
+};
+
+/** @type {import('esbuild').BuildOptions}*/
+const pluginNode = {
+    ...common,
+    entryPoints: ['src/plugin/index-node.ts'],
+    outfile: outDir + '/plugin/index-node.js',
+    platform: 'node',
+    packages: 'external',
+};
+
+/** @type {import('esbuild').BuildOptions}*/
+const runtime = {
+    ...common,
+    entryPoints: ['src/runtime/index.ts'],
+    outfile: outDir + '/runtime/index.js',
+    minify: true,
+    platform: 'browser',
+    plugins: [sassPlugin()],
+};
+
+build(plugin);
+build(pluginNode);
+build(runtime);
---
+++ src/plugin/index.ts
@@ -0,0 +1,13 @@
+export type {PluginOptions, TransformOptions} from './transform';
+export {transform} from './transform';
+export {
+    FileClassName,
+    FileSpecialAttr,
+    LinkHtmlAttr as FileHtmlAttr,
+    FILE_TOKEN,
+    FILE_TO_LINK_ATTRS_MAP,
+    RULE_NAME as FILE_RULE_NAME,
+    KNOWN_ATTRS as FILE_KNOWN_ATTRS,
+    REQUIRED_ATTRS as FILE_REQUIRED_ATTRS,
+    PREFIX as FILE_MARKUP_PREFIX,
+} from './const';
